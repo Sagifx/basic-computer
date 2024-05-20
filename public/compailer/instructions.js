ad4691fa85fbbd4c2163df3be6b2e0c5cff4b335
Sagi <sagifx@gmail.com>
2024-05-20 19:38:42 +0300

    reorgnize for firebase

---
+++ public/compailer/instructions.js
@@ -0,0 +1,297 @@
+/**
+ * add zeros to complete accumulator to 16 bit
+ * @param arg (bin)
+ * @returns arg (bin) 16 bit
+ */
+function padding(arg, paddingTo) {
+    if (arg)
+        while (arg.length < paddingTo) arg = "0" + arg;
+    return arg;
+}
+
+/**check accumulator deviation
+ * get accumulator future value (16/17 bit) binary base
+ * @param arg (bin)
+ * @returns boolean
+ */
+function deviation(arg) {
+    //return ((arg.length == 17 && arg[0] == "1") ? true : false);
+    return arg.length == 17; //true or false
+}
+
+// increment accumulator
+/**
+ * convert to dec and add 1 to the val
+ * convert to bin and add padding
+ * slice first char if reconize diviation
+ * placement binary new val at acReg (global variable)
+ */
+function INC() {
+    let arg = parseInt("0x" + (acReg), 16); // parse string to int from 0x hex
+    arg++;
+    arg = dec2bin(arg);
+    arg = padding(arg, 16);
+    if (deviation(arg)) {
+        eFlag = "1";
+        arg = arg.slice(1, arg.length + 1);
+    }
+    acReg = bin2hex(arg);
+    pc++;
+}
+
+// clear accumulator
+function CLA() {
+    acReg = "0000";
+    pc++;
+}
+
+// clear E flag
+function CLE() {
+    eFlag = "0";
+    pc++;
+}
+
+// compliment AC
+function CMA() {
+    let accumulator = "";
+    let binAC = hex2bin(acReg); // bin and padding to 16
+    //binAC = padding(binAC, 16);
+    binAC = binAC.split("");
+    binAC.forEach(bit => {
+        bit = Number(!Number(bit)); // compliment every bit
+        accumulator += bit; // add the compliment bit to the new value of the ac
+    });
+    acReg = bin2hex(accumulator); // placement the new value at the ac reg
+    pc++;
+}
+
+// compliment E flag
+function CME() {
+    let e = Number(eFlag);
+    eFlag = Number(!e).toString(2); // place the compliment value at the eFlag
+    pc++;
+}
+
+// circulate left accumulator
+function CIL() {
+    let accumulator = hex2bin(acReg);
+    accumulator = padding(accumulator);
+    accumulator = accumulator + eFlag; // 17 chars string
+    eFlag = accumulator.slice(0, 1); // took the first char tot he eFlag (after circulate)
+    accumulator = accumulator.slice(1, accumulator.length); // took the rest of the chars tot he ac
+    acReg = bin2hex(accumulator); // placement the new ac val into the acReg global var
+    pc++;
+}
+
+// circulate right accumulator, as same logic as the CIL instruction
+function CIR() {
+    let accumulator = hex2bin(acReg);
+    accumulator = padding(accumulator);
+    accumulator = eFlag + accumulator;
+    eFlag = accumulator.slice(accumulator.length - 1, accumulator.length);
+    accumulator = accumulator.slice(0, accumulator.length - 1);
+    acReg = bin2hex(accumulator);
+    pc++;
+}
+
+/**
+ * add between accumulator and value in the memory
+ * get the memory value as argument
+ */ 
+function ADD(mar) {
+    let accumulator = parseInt("0x" + (acReg), 16);
+    mar = bin2dec(mar);
+    accumulator = accumulator + Number(mar);
+    accumulator = String(accumulator);
+    accumulator = dec2bin(accumulator);
+    if (deviation(accumulator)) {
+        eFlag = "1";
+        accumulator = accumulator.slice(1, accumulator.length + 1);
+    }
+    acReg = bin2hex(accumulator);
+    pc++;
+}
+
+/** 
+ * and between accumulator and value in the memory
+ * get the memory value 
+ */
+function AND(mar) {
+    let accumulator = "";
+    let acArr = hex2bin(acReg);
+    mar = padding(mar, 16);
+    acArr = padding(acArr);
+    for (let i = 0 ; i < 16 ; i++ ) {
+        accumulator += ((mar[i] == "1" && acArr[i] == "1") ? "1" : "0");
+    }
+    acReg = bin2hex(accumulator);
+    pc++;
+}
+
+/* load to accumulator
+   get memory value */
+function LDA(mar) {
+    acReg = bin2hex(mar);
+    pc++;
+}
+
+/* store accumulator in the memory
+   get the address to store at */
+function STAa(address) {
+    let base = $(`#row${address}`)[0].getElementsByClassName("instruction-cmd-input")[0].value;
+    let val = acReg;
+    if (base == "DEC")
+        val = hex2dec(acReg);
+    else if (base == "BIN")
+        val = hex2bin(acReg);
+    $(`#row${address}`)[0].getElementsByClassName("value-input")[0].value = val;
+    //$(`#row${address}`)[0].getElementsByClassName("label-cmd-input")[0].value = "HEX";
+    storeDataInJson(address);
+    pc++;
+}
+
+function STA(address) {
+    let val = acReg;
+    hardStoreDataInJson(address, val);
+    let firstFlag = hex2dec($("#org-value")[0].value);
+    let lastFlag = hex2dec(document.getElementById("cmd-container").lastChild.getElementsByClassName("count-address")[0].innerHTML); //get the address of the last row
+    //$(`#row${address}`)[0].getElementsByClassName("value-input")[0].value = val;
+    //fetchCmd2Memory();
+    fetchMemory2Cmd(firstFlag, lastFlag);
+    pc++;
+}
+
+/* branch unconditionally 
+get the address */
+function BUN(address) {
+    pc = hex2dec(address);
+}
+
+/* branch and save address
+get the address and store there the address to back */
+function BSA(address) {
+    let backAddress = dec2hex(Number(pc) + 1);
+    let subAddress = $(`#row${address}`)[0]; // subrutin address
+    if (subAddress.getElementsByClassName("instruction-cmd-input")[0].value == "DEC")
+        backAddress = Number(pc) + 1;
+    subAddress.getElementsByClassName("value-input")[0].value = backAddress;
+    storeDataInJson(address);
+    pc = (Number(hex2dec(address)) + 1);
+}
+
+/* increment and skip if zero
+get address */
+function ISZ(address) {
+    let row = $(`#row${address}`)[0];
+    let val = row.getElementsByClassName("value-input")[0].value;
+    let isHEX;
+    isHEX = row.getElementsByClassName("instruction-cmd-input")[0].value == "HEX";
+    isHEX ? val = hex2dec(val) : null;
+    val = Number(val) + 1;
+    row.getElementsByClassName("value-input")[0].value = isHEX ? dec2hex(val) : val;
+    storeDataInJson(address);
+    val == 0 ? pc++ : null;
+    pc++;
+}
+
+// skip if accumulator positive (or zero)
+function SPA() {
+    let binAC = hex2bin(acReg).split("");
+    binAC[0] == 0 ? pc++ : null;
+    pc++;
+}
+
+// skip if accumulator negetive
+function SNA() {
+    let binAC = hex2bin(acReg).split("");
+    binAC[0] == 1 ? pc++ : null;
+    pc++;
+}
+ // skip if accumulators value is 0
+function SZA() {
+    acReg == 0 ? pc++ : null;
+    pc++;
+}
+
+// skip if the E flag is 0
+function SZE() {
+    eFlag == 0 ? pc++ : null;
+    pc++;
+}
+
+// if the interupt on the input accumulator get the input register value (8 bit)
+function INP() {
+    //if ($("#interupt-enable")[0].innerHTML == "ON") {
+        let val = document.getElementById("input-register").value;
+        acReg = acReg.slice(0, 2) + val;
+    //} else {
+    //    console("Interupt disable");
+    //}
+    pc++;
+}
+
+// if the interupt off the output register get the accumulator value (8 bit)
+function OUT() {
+    let val = acReg.slice(2,4);
+    $("#output-register")[0].innerHTML = val;
+    pc++;
+}
+
+// interupt on
+function ION() {
+    $("#interupt-enable")[0].innerHTML = "ON";
+    pc++;
+}
+
+// interupt off
+function IOF() {
+    $("#interupt-enable")[0].innerHTML = "OFF";
+    pc++;
+
+}
+
+// skip if the input flag on
+function SKI() {
+    let isChecked = $("#input-checkbox")[0].checked;
+    if (isChecked) {
+        $("#input-checkbox")[0].checked = false;
+        pc++;
+    }
+    pc++;
+}
+
+async function runSKI() {
+    if ($("#input-checkbox")[0].checked) {
+        $("#input-checkbox")[0].checked = false;
+        pc = Number(pc) + 2;
+        return false;
+    } else {
+       await sleep(500);
+    }
+}
+
+// skip if the output flag on
+function SKO() {
+    isChecked = $("#output-checkbox")[0].checked;
+    if (isChecked) {
+        $("#output-checkbox")[0].checked = false;
+        pc++;
+    }
+    pc++;
+}
+
+async function runSKO() {
+    if ($("#output-checkbox")[0].checked) {
+        $("#output-checkbox")[0].checked = false;
+        pc += 2;
+        return false;
+    } else {
+        await sleep(500);
+    }
+}
+
+
+// solution for waiting for interrupt
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
---
+++ public/compailer/running.js
@@ -0,0 +1,345 @@
+/**
+ * triggered by 'run the code' btn
+ * run all the program
+ */
+async function runStep() {
+    step = true;
+    await exe();
+}
+
+/**
+ * triggered by 'step by step' btn
+ * run one line at the program
+ */
+async function run() {
+    step = false;
+    await exe();
+}
+
+/**
+ * execute the program
+ * looking for HLT and END, restart values and collect label
+ * running according to the user choise (run or step by step)
+ * and with switch case execute the instructions 
+ */
+async function exe() {
+    if (pc == hex2dec($("#org-value")[0].value) && !lastIndex) { // if true its the first run
+        if (!lookForHLTandEND()) { // stop running if missing HLT ot END
+            console("Missing HLT or END");
+            return;
+        }
+        lastIndex = "";
+        //collectLabels(); // creat Json of labels (as key) and addresses (as values)
+        checkInputs();
+        fetchCmd2Memory(); // fetch the cmd data to the memoryJson
+    }
+    acReg = $("#ac-value")[0].value; //acReg value
+    eFlag = $("#E-value")[0].value; //carry flag value
+    //$("#org-value").keyup((e) => listenToOrg(e)); //change the adresses by changing the org
+    let val; //hold the input value without the I
+    let I;
+    let index = dec2hex(pc).slice(1, 4);
+    let rowElem = $(`#row${index}`)[0];
+    let currentInstruction = rowElem.getElementsByClassName("instruction-cmd-input")[0].value;
+    while (currentInstruction != "HLT") {
+        val = rowElem.getElementsByClassName("value-input")[0].value;
+        I = val.split(" ")[1] == "I" ? true : false; //save true if indirect
+        val = val.split(" ")[0]; //value without the 'I'
+        console("");
+        switch (currentInstruction) {
+            // memory instructions
+            case "AND":
+                AND(getValueByAddress(labelToAddress(val, I)));   //arg- bin value at address (I)
+                break;
+            case "ADD":
+                ADD(getValueByAddress(labelToAddress(val, I)));  //arg- bin value at address (I)
+                break;
+            case "LDA":
+                LDA(getValueByAddress(labelToAddress(val, I)));  //get value from label to ac
+                break;
+            case "STA":
+                STA(labelToAddress(val, I));  //store value from ac to label
+                break;
+            case "BUN":
+                BUN(labelToAddress(val, I));  //the val will be address
+                break;
+            case "BSA":
+                BSA(labelToAddress(val, I));  //the val will be address
+                break;
+            case "ISZ":
+                ISZ(labelToAddress(val, I)); //?
+                break;
+            // register instructions
+            case "CLA":
+                CLA();  //ac
+                break;
+            case "CLE":
+                CLE();  //ac
+                break;
+            case "CMA":
+                CMA();  //ac
+                break;
+            case "CME":
+                CME();  //e flag
+                break;
+            case "CIR":
+                CIR();  //ac
+                break;
+            case "CIL":
+                CIL();  //ac
+                break;
+            case "INC":
+                INC();  //ac
+                break;
+            case "SPA":
+                SPA();  //ac
+                break;
+            case "SNA":
+                SNA();  //ac
+                break;
+            case "SZA":
+                SZA();  //ac
+                break;
+            case "SZE":
+                SZE();
+                break;
+            // input/output intructions
+            case "INP":
+                INP();
+                break;
+            case "OUT":
+                OUT();
+                break;
+            case "SKI":
+                console("Waiting for input flag");
+                if (!step && ($(`#row${index}`)[0].getElementsByClassName("label-cmd-input")[0].value ==
+                    $(`#row${dec2hex(Number(hex2dec(index)) + 1).slice(1,)}`)[0].getElementsByClassName("value-input")[0].value) &&
+                    $(`#row${dec2hex(Number(hex2dec(index)) + 1).slice(1,)}`)[0].getElementsByClassName("instruction-cmd-input")[0].value == "BUN") {
+                    while (await runSKI());
+                }
+                else SKI();
+                break;
+            case "SKO":
+                console("Waiting for output flag");
+                if (!step && ($(`#row${index}`)[0].getElementsByClassName("label-cmd-input")[0].value ==
+                    $(`#row${dec2hex(Number(hex2dec(index)) + 1).slice(1,)}`)[0].getElementsByClassName("value-input")[0].value) &&
+                    $(`#row${dec2hex(Number(hex2dec(index)) + 1).slice(1,)}`)[0].getElementsByClassName("instruction-cmd-input")[0].value == "BUN") {
+                    while (await runSKO());
+                }
+                else SKO();
+                break;
+            case "ION":
+                ION();   // R flag
+                break;
+            case "IOF":
+                IOF();   // R flag
+                break;
+            case "BIN":
+                pc++;
+                break;
+            case "DEC":
+                pc++;
+                break;
+            case "HEX":
+                pc++;
+                break;
+            default:
+                console("Missing row in the flow");
+                return;
+        }
+        lastIndex ? $(`#row${lastIndex}`)[0].getElementsByClassName("address")[0].style.backgroundColor = "transparent" : null;
+        $(`#row${index}`)[0].getElementsByClassName("address")[0].style.backgroundColor = "yellow";
+        lastIndex = index;
+        index = dec2hex(pc).slice(1, 4); //get the new index after pc was change 
+        rowElem = $(`#row${index}`)[0];
+        currentInstruction = rowElem.getElementsByClassName("instruction-cmd-input")[0].value;
+        $("#ac-value")[0].value = acReg;
+        $("#E-value")[0].value = eFlag;
+        $("#pc")[0].innerHTML = dec2hex(pc).slice(1, 4);
+        $("#run-btn")[0].innerHTML = "Keep running";
+        convertToMachineLang();
+        if (step) return; //if (step && currentInstruction != "HLT") return;
+    }
+    $(`#row${index}`)[0].getElementsByClassName("address")[0].style.backgroundColor = "yellow";
+    $(`#row${lastIndex}`)[0].getElementsByClassName("address")[0].style.backgroundColor = "transparent";
+    currentInstruction == "HLT" ? console("The program finished by HLT") : null;
+    pc = hex2dec($("#org-value")[0].value);
+    lastIndex = "";
+    $("#run-btn")[0].innerHTML = "Run the code";
+}
+
+/** 
+ * look for HLT in the program
+ * without halt the program shouldnt run and the error message will appear in the console
+ */ 
+function lookForHLTandEND() {
+    let rows = Array.from($(".instruction-cmd-input"));
+    let findHLT = false;
+    let findEND = false;
+    rows.forEach(r => {
+        if (r.value == "HLT") {
+            r.parentElement.parentElement.getElementsByClassName("address")[0].style.backgroundColor = "transparent";
+            findHLT = true;
+        }
+        if (r.value == "END") {
+            r.parentElement.parentElement.getElementsByClassName("address")[0].style.backgroundColor = "transparent";
+            endFlag = r.parentElement.parentElement.getElementsByClassName("count-address")[0].innerHTML;
+            findEND = true;
+        }
+    });
+    if (findEND && findHLT) return true;
+    return false;
+}
+
+// print msg to the user
+function console(txt) {
+    $("#console")[0].innerHTML = txt;
+}
+
+// print msg to the user
+function converterConsole(txt) {
+    $("#converterConsole")[0].innerHTML = txt;
+}
+
+
+
+
+/* MACHINE LANGUAGE                                                      */
+ 
+
+// convert the instructions and value to machine lang
+function convertToMachineLang() {
+    let rows = Array.from($(".general-row"));
+    let machineLang;
+    let val;
+    let I;
+    collectLabels();
+    rows.forEach(r => {
+        currentInstruction = r.getElementsByClassName("instruction-cmd-input")[0].value;
+        val = r.getElementsByClassName("value-input")[0].value;
+        if (currentInstruction === ("DEC" || "BIN" || "HEX")) {
+            /**
+             * for value in memory the value need to convert to hex
+             * if the value is negetive it should: 1. convert to dec
+             *                                     2. 2's complement (back as bin)
+             *                                     3. convert to hex
+             */
+            switch (currentInstruction) {
+                case "BIN":
+                    machineLang = (val < 0) ? bin2hex(complement(bin2dec(val))) : bin2hex(val); 
+                    break;
+                case "DEC":
+                    machineLang = (val < 0) ? bin2hex(complement(val)) : dec2hex(val);
+                    break;
+                case "HEX":
+                    machineLang = (val < 0) ? bin2hex(complement(hex2dec(val))) : val;
+                    break;
+            }
+        }
+        else {
+            val = val.split(" ")[0];
+            I = val.split(" ")[1];
+            switch (currentInstruction) {
+                // memory
+                case "AND":
+                    machineLang = (I ? '8' : '0') + padding(labelToAddress(val, I), 3);
+                    break;
+                case "ADD":
+                    machineLang = (I ? '9' : '1') + padding(labelToAddress(val, I), 3);
+                    break;
+                case "LDA":
+                    machineLang = (I ? 'A' : '2') + padding(labelToAddress(val, I), 3);
+                    break;
+                case "STA":
+                    machineLang = (I ? 'B' : '3') + padding(labelToAddress(val, I), 3);
+                    break;
+                case "BUN":
+                    machineLang = (I ? 'C' : '4') + padding(labelToAddress(val, I), 3);
+                    break;
+                case "BSA":
+                    machineLang = (I ? 'D' : '5') + padding(labelToAddress(val, I), 3);
+                    break;
+                case "ISZ":
+                    machineLang = (I ? 'E' : '6') + padding(labelToAddress(val, I), 3);
+                    break;
+                // register
+                case "CLA":
+                    machineLang = "7800";
+                    break;
+                case "CLE":
+                    machineLang = "7400";
+                    break;
+                case "CMA":
+                    machineLang = "7200";
+                    break;
+                case "CME":
+                    machineLang = "7100";
+                    break;
+                case "CIR":
+                    machineLang = "7080";
+                    break;
+                case "CIL":
+                    machineLang = "7040"
+                    break;
+                case "INC":
+                    machineLang = "7020";
+                    break;
+                case "SPA":
+                    machineLang = "7010";
+                    break;
+                case "SNA":
+                    machineLang = "7008";
+                    break;
+                case "SZA":
+                    machineLang = "7004";
+                    break;
+                case "SZE":
+                    machineLang = "7002";
+                    break;
+                // input output
+                case "INP":
+                    machineLang = "F800";
+                    break;
+                case "OUT":
+                    machineLang = "F400";
+                    break;
+                case "SKI":
+                    machineLang = "F200";
+                    break;
+                case "SKO":
+                    machineLang = "F100";
+                    break;
+                case "ION":
+                    machineLang = "F080";
+                    break;
+                case "IOF":
+                    machineLang = "F040";
+                    break;
+                case "HLT":
+                    machineLang = "7001";
+                    break;
+                default:
+                    machineLang = "";
+                    break;
+            }
+        }
+        r.getElementsByClassName("machine-lang")[0].innerHTML = machineLang;
+        if ($(`#show-machine-lang`)[0].innerHTML.includes("Show"))
+            r.getElementsByClassName("machine-lang")[0].style.color = "transparent";
+    });  
+}
+
+ // toggle between show and hide machine lang by show/hide btn
+function showMachineLangToggle() {
+    let btn = $(`#show-machine-lang`)[0];
+    let rows = Array.from($(".machine-lang"));
+    rows.forEach(r => {
+        if (btn.innerHTML == "Show machine language") {
+            r.style.color = "black";
+        }
+        else {
+            r.style.color = "transparent";
+        }
+    });
+    btn.innerHTML = (btn.innerHTML.includes("Show") ? "Hide" : "Show") + " machine language";
+}
